4a900ca50107ff565598c54919e12c0f554317ac
Michael <oliengo.michael@gmail.com>
2024-06-28 19:34:59 +0300

    cmplete loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    countdown = 10
+    
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    
+    print("Happy New Year!")
+    
     pass
 
-def square_integers(int_list):
-    # code goes here!
+def square_integers(nums):
+    squared_nums = [num ** 2 for num in nums]
+    return squared_nums
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
